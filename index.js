a5132ea9beb136cf5db71d37ec0e3d5d18ebf81c
John Garbero <john.garbero.itb@gmail.com>
2022-05-17 12:32:36 -0500

    commit tree ed89f2335e573168feb99f0c5a72881027074e77

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // code your solution here
+
+// function superbowlWin(record) {
+//     for (const result of record) {
+//         record.find(result === "W") {
+//             return year
+//         }
+//     }
+// }
+
+// function superbowlWin(record) {
+//     record.find(year =result === "W")
+// }
+
+// const win = () => result === "W"
+
+// const superbowlWin = record.find(win)
+
+// function superbowlWin(record) {
+//     return (year if (result ==="W"))
+// }
+
+// const superbowlWin = record.find(function (year) {
+//     return record === "W"
+// )}
+
+// const superbowlWin = record.find( (year) => result === "W");
+
+// function superbowlWin(record) {
+    
+// }
+
+// function win(x) {
+//     if (result === "W") {
+//         return year
+//     }
+// }
+
+// function superbowlWin(record) {
+//     record.find(win);
+// }
+
+// function superbowlWin(record) {
+//     FileSystemHandle((result, record) => result = "W")
+// }
+
+// function superbowlWin(record){
+//     record.find(season => season.result === 'W')
+// }
+
+function superbowlWin(array){
+    const newObj = array.find(array => array.result === 'W')
+    return newObj ? newObj.year : undefined
+}
